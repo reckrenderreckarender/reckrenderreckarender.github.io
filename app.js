ba28d239e5f716ec5be451178c86b989b700d6f3
Sebeck Yuiomen <117760769+reckrenderreckarender@users.noreply.github.com>
2025-07-17 02:25:32 +0000

    sidebar, no content yet, responsive

---
+++ app.js
@@ -0,0 +1,9 @@
+const toggleButton = document.getElementById('toggle-btn')
+const sidebar = document.getElementById('sidebar')
+
+function toggleSidebar(){
+  sidebar.classList.toggle('close')
+  toggleButton.classList.toggle('rotate')
+
+  closeAllSubMenus()
+}
